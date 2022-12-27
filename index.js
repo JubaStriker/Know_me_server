9d428595c69e3a437b05b671fe83a36f9442c0aa
JubaStriker <jubastriker@gmail.com>
2022-12-27 19:22:26 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
-
+require('dotenv').config();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = process.env.DB_USER;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', async (req, res) => {
     res.send('know me server is running');
